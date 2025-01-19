62e9ce4b2c129674185818c299d0406485b9b2fa
abrormukhammadiev <abrormukhammadiev@gmail.com>
2025-01-20 04:07:03 +0500

    feat: products page table implemented

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,36 +1,70 @@
-import {Component, signal} from '@angular/core';
-import {AuthService} from '../login/auth/auth.service';
-import {Product} from '../shared/types';
+import { Component, signal, effect } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { MatTableModule } from '@angular/material/table';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 import {
-  MatCard,
-  MatCardActions,
-  MatCardAvatar,
-  MatCardContent,
-  MatCardHeader,
-  MatCardSubtitle,
-  MatCardTitle
-} from '@angular/material/card';
-import {MatButton} from '@angular/material/button';
+  animate,
+  state,
+  style,
+  transition,
+  trigger
+} from '@angular/animations';
 
+import { AuthService } from '../login/auth/auth.service';
+import { Product } from '../shared/types';
 @Component({
   selector: 'app-product-list',
   imports: [
-    MatCardContent,
-    MatCardTitle,
-    MatCardSubtitle,
-    MatCardActions,
-    MatCardHeader,
-    MatCard
+    CommonModule,
+    FormsModule,
+    MatTableModule,
+    MatIconModule,
+    MatButtonModule
   ],
   templateUrl: './product-list.component.html',
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({ height: '0px', minHeight: '0' })),
+      state('expanded', style({ height: '*' })),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)'))
+    ])
+  ],
   styleUrl: './product-list.component.css'
 })
 export class ProductListComponent {
   productList = signal<Product[]>([]);
+  columnsToDisplay = ['id', 'sku', 'name', 'cost'];
+  columnsToDisplayWithExpand = [...this.columnsToDisplay, 'expand'];
+  expandedElement = signal<Product | null>(null);
+  editingId = signal<number | null>(null);
 
   constructor(private authService: AuthService) {
+    effect(() => {
+      console.log(this.expandedElement())
+    });
     this.authService.fetchProducts().subscribe((data: Product[]) => {
+      console.log(data)
       this.productList.set(data)
+
     })
+  }
+
+  startEditing(element: Product) {
+    this.editingId.set(element.id);
+  }
+
+// Optionally, handle saving or cancelling:
+  saveEdit(element: Product) {
+    // TODO: call a service to PATCH or otherwise update the product
+    console.log('Saving changes for', element);
+
+    // Once saved, exit edit mode
+    this.editingId.set(null);
+  }
+
+  cancelEdit() {
+    this.editingId.set(null);
   }
 }
