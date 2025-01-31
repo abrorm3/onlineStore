5ba2a6b5d22ab563601a645c6f7ec7a7eba910af
abrormukhammadiev <abrormukhammadiev@gmail.com>
2025-01-31 05:00:20 +0500

    fix: set loader only on fetch (GET)

--- src/app/shared/loader.interceptor.ts
+++ src/app/shared/loader.interceptor.ts
@@ -1,15 +1,18 @@
-import { HttpInterceptorFn } from '@angular/common/http';
-import { inject } from '@angular/core';
-import { finalize } from 'rxjs/operators';
-import { SharedService } from './shared.service';
+import {HttpInterceptorFn} from '@angular/common/http';
+import {inject} from '@angular/core';
+import {finalize} from 'rxjs/operators';
+import {SharedService} from './shared.service';
 
 export const loaderInterceptor: HttpInterceptorFn = (req, next) => {
-  const sharedService = inject(SharedService);
-  sharedService.isLoading.set(true);
-
-  return next(req).pipe(
-    finalize(() => {
-      sharedService.isLoading.set(false);
-    })
-  );
+    const sharedService = inject(SharedService);
+    if (req.url.includes('/items') && req.method === 'GET') {
+        sharedService.isLoading.set(true);
+        return next(req).pipe(
+            finalize(() => {
+                sharedService.isLoading.set(false);
+            })
+        );
+    } else {
+        return next(req);
+    }
 };
